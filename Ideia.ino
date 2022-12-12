9230a39e0b53accd29bff0a24a81f5ba2e4285a9
Lui Gill <61015032+luigill@users.noreply.github.com>
2022-12-12 09:55:23 -0300

    Add files via upload

---
+++ Ideia.ino
@@ -0,0 +1,50 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+String letraEsperada = "71 8A AE A9";
+ 
+//char st[20];
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Inicia a serial
+  SPI.begin();      // Inicia  SPI bus
+  mfrc522.PCD_Init();   // Inicia MFRC522
+  Serial.println("Aproxime a letra:");
+  Serial.println();
+}
+ 
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  Serial.print("UID da tag :");
+  String conteudo= "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  conteudo.toUpperCase();
+  if (conteudo.substring(1) == letraEsperada)
+  {
+    Serial.println("Letra A");
+    Serial.println();
+    delay(3000);
+  }
+} 
